26ae412070c1b85951420d707d28a32a44ae1208
10056232 <10056232@NOI-3F3F0N2.gemalto.com>
2020-12-22 17:45:34 +0530

    La la la

---
+++ hb-05-many-to-many/src/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -0,0 +1,136 @@
+package com.luv2code.hibernate.demo.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor")
+public class Instructor {
+	
+	// annotate the class as an entity and map to db table
+	
+	// define the fields
+		
+	// annotate the fields with db column names
+		
+	// setup mapping to InstructorDetail entity
+	
+	// create constructors
+		
+	// generate getter/setter methods
+		
+	// generate toString() method
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstname;
+	
+	@Column(name="last_name")
+	private String lastname;
+	
+	@Column(name="email")
+	private String email;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="instructor_detail_id")
+	private InstructorDetail instructorDetail;
+	
+	@OneToMany(fetch=FetchType.LAZY, mappedBy="instructor", 
+			cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+				      CascadeType.REFRESH, CascadeType.DETACH})
+	private List<Course> courses;
+
+	public Instructor() {
+
+	}
+
+	public Instructor(String firstname, String lastname, String email) {
+		super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public InstructorDetail getInstructorDetail() {
+		return instructorDetail;
+	}
+
+	public void setInstructorDetail(InstructorDetail instructorDetail) {
+		this.instructorDetail = instructorDetail;
+	}
+	
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", email=" + email
+				+ ", instructorDetail=" + instructorDetail + "]";
+	}
+	
+	// add convenience methods for bi directional relationship
+	
+	public void add(Course tempCourse) {
+		
+		if(courses == null) {
+			courses = new ArrayList<>();
+		}
+		
+		courses.add(tempCourse);
+		
+		tempCourse.setInstructor(this);
+	}
+	
+}
